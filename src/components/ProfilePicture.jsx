badc5b414c4dc6d8c22783ae3d77581794d34202
Ali Almasi <A710almasi@gmail.com>
2025-02-26 16:33:38 +0330

    integrated profilepicture with clsx

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import source from "/public/me.webp";
-
-const style = "aspect-square select-none";
+import clsx from "clsx";
 
 export default function ProfilePicture({ size, circle = false }) {
   return (
     <Image
       src={source}
       alt={`${process.env.author}'s Profile Picture`}
-      className={circle ? `rounded-full ${style}` : style}
+      className={clsx("aspect-square select-none", circle && "rounded-full")}
       width={size}
       height={size}
       placeholder="blur"
